ed265f6f9dfc6d1174ac6cc486440ee817eef52e
Dominic Else <dominic.else@gmail.com>
2020-02-23 21:52:23 -0500

    Restore a missing '%errhnd' in make_env_windows.bat

--- make_env_windows.bat
+++ make_env_windows.bat
@@ -8,7 +8,7 @@ py -3 --version >nul %errhnd%
 mkdir imbibe_env %errhnd%
 py -3 -m venv imbibe_env %errhnd%
 call imbibe_env\Scripts\activate.bat %errhnd%
-pip3 install wheel
+pip3 install wheel %errhnd%
 pip3 install arxiv habanero unidecode %errhnd%
 xcopy imbibe imbibe_env\Lib\site-packages\imbibe\ %errhnd%
 
